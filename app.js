e534d2aad53b74e88d7e637c63d6ca57876252a9
Cole Allen <decolesss@gmail.com>
2017-06-07 01:29:44 -0700

    class-5-lab: problem 1 solved

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+function sum(testSum) {
+  if (testSum === 11);
+    return ("The sum of 4 and 7 is 11.");
+}
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
